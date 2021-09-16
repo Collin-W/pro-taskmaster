00b5806ed88aeb3678b100776c0ba2d6e6572949
Collin-W <whalencollin@gmail.com>
2021-09-16 16:56:25 -0500

    drag and drop ui

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -1,6 +1,57 @@
 var tasks = {};
 
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    // array to store the task data in
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      // trim down list's ID to match object property
+      var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+      // update array on tasks object and save
+      tasks[arrName] = tempArr;
+      saveTasks();
+
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    console.log(tempArr);
+  }
+  });
+
 // due date was clicked
 $(".list-group").on("click", "span", function() {
   // get current text
@@ -26,7 +77,7 @@ $(".list-group").on("blur", "input[type='text']", function() {
   // get current text
   var date = $(this)
     .val()
-    .trim();
+    ;
 
   // get the parent ul's id attribute
   var status = $(this)
@@ -192,6 +243,23 @@ $("#remove-tasks").on("click", function() {
     $("#list-" + key).empty();
   }
   saveTasks();
+});
+
+
+$("#trash").droppable({
+  accept:".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+
 });
 
 // load tasks for the first time
