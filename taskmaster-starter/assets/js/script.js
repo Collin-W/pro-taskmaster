3b9fa174babdb505c828b680b493e69cb34a5bbd
Collin-W <whalencollin@gmail.com>
2021-09-20 16:49:58 -0500

    UX enhancements

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -10,16 +10,23 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    console.log("activate", this);
+    $(this) .addClass("dropover")
+    $(".bottom-trash").addClass("bottom-trash-drag");
+    
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    $(this) .removeClass("dropover")
+    $(".bottom-trash").removeClass("bottom-trash-drag");
+
+    
   },
   over: function(event) {
-    console.log("over", event.target);
+   $(event.target).addClass("dropover-active");
+  
+
   },
   out: function(event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
     // array to store the task data in
@@ -233,7 +240,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -272,9 +279,11 @@ $("#trash").droppable({
     console.log("drop");
   },
   over: function(event, ui) {
+    $(".bottom-trash").addClass("bottom-trash-active");
     console.log("over");
   },
   out: function(event, ui) {
+    $(".bottom-trash").remove("bottom-trash-active");
     console.log("out");
   }
 });
@@ -300,6 +309,14 @@ var auditTask = function(taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
 };
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+    console.log(el)
+  });
+}, (1000 * 60) * 30);
+//1800000
 
 // load tasks for the first time
 loadTasks();
